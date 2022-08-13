044d8d71c11ef6fad83c37ae52bc58c557824a77
joaocmd <joaodavid1999@hotmail.com>
2022-08-13 21:36:55 +0100

    Add DFP with 1x1 conv on last layer

--- floortrans/models/__init__.py
+++ floortrans/models/__init__.py
@@ -1,5 +1,6 @@
 from floortrans.models.hg_furukawa_original import *
-from floortrans.models.dfp_adapted import *
+from floortrans.models.dfp_adapted import DFPmodel
+from floortrans.models.dfp_adapted_conv import DFPConvModel
 
 def get_model(name, n_classes=None, version=None):
     if name == 'hg_furukawa_original':
@@ -7,6 +8,8 @@ def get_model(name, n_classes=None, version=None):
         # model.init_weights()
     elif name == 'dfp':
         model = DFPmodel(pretrained=True, freeze=False, n_classes=n_classes)
+    elif name == 'dfp-last-conv':
+        model = DFPConvModel(pretrained=True, freeze=False, n_classes=n_classes)
     else:
         raise ValueError('Model {} not available'.format(name))
 
