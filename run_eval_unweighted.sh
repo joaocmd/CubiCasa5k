5b09a3abfcc0adb41eeddf0e5f6c79539c651958
PastelBelem8 <catarina.belem@ist.utl.pt>
2022-09-06 21:34:26 -0700

    Fix args in run_eval_unweighted.sh

--- run_eval_unweighted.sh
+++ run_eval_unweighted.sh
@@ -10,11 +10,13 @@ do
 done
 
 echo "Running evaluation"
-for FILE in "baseline" "2022-08-09-10 51 42-dfp-experiment" "2022-08-14-09 16 10-dfp-last-conv-experiment" "2022-08-25-11 35 22-dfp-resnet34-conv" "2022-08-25-11 35 22-dfp-resnet50-conv" "2022-08-26-10 22 12-deeplabv3" "2022-08-29-23 30 37-dfp-resnet34-encoder-conv"
-do 
-    if test -f "experiment1-uncertainty-loss/$FILE/model_best_val_loss_var.pkl"; then
-        python eval.py --weights "experiment1-uncertainty-loss/$FILE/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/$FILE"
-    else
-        echo "Couldn't run for $FILE"
-    fi
-done
+# python eval.py --arch hg_furukawa_original --weights "experiment1-uncertainty-loss/baseline/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/baseline"
+
+python eval.py --arch "dfp" --weights "experiment1-uncertainty-loss/2022-08-09-10 51 42-dfp-experiment/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/2022-08-09-10 51 42-dfp-experiment"
+python eval.py --arch "dfp-last-conv" --weights "experiment1-uncertainty-loss/2022-08-14-09 16 10-dfp-last-conv-experiment/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/2022-08-14-09 16 10-dfp-last-conv-experiment"
+python eval.py --arch "dfp-resnet34-conv" --weights "experiment1-uncertainty-loss/2022-08-25-11 35 22-dfp-resnet34-conv/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/2022-08-25-11 35 22-dfp-resnet34-conv"
+python eval.py --arch "dfp-resnet34-encoder-conv" --weights "experiment1-uncertainty-loss/2022-08-29-23 30 37-dfp-resnet34-encoder-conv/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/2022-08-29-23 30 37-dfp-resnet34-encoder-conv"
+python eval.py --arch "dfp-resnet50" --weights "experiment1-uncertainty-loss/2022-08-25-11 35 22-dfp-resnet50-conv/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/2022-08-25-11 35 22-dfp-resnet50-conv"
+python eval.py --arch "deeplabv3" --weights "experiment1-uncertainty-loss/2022-08-26-10 22 12-deeplabv3/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/2022-08-26-10 22 12-deeplabv3"
+
+# python eval.py --arch "$FILE" --weights "experiment1-uncertainty-loss/$FILE/model_best_val_loss_var.pkl" --output-dir "output/results/experiment1-uncertainty-loss/$FILE"
