39a4d0aeb4b48f3cdc4174e8327d5fae0c99aea2
PastelBelem8 <catarina.belem@ist.utl.pt>
2022-09-09 17:37:44 -0700

    Update eval_weighted

--- run_eval_weighted.sh
+++ run_eval_weighted.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Checking whether directory exists"
-for FILE in "dfp" "dfp-last-conv" "dfp-resnet34-conv" "dfp-resnet50-conv" "deeplabv3" "dfp-resnet34-encoder-conv"
+for FILE in "dfp" "dfp-last-conv" "dfp-resnet34" "dfp-resnet50" "deeplabv3" "dfp-resnet34-encoder-conv"
 do 
     if test -f "experiment2-weighted-loss/$FILE/model_best_val_loss_var.pkl"; then
         echo
@@ -13,8 +13,7 @@ echo "Running evaluation"
 
 python eval.py --arch "dfp" --weights "experiment2-weighted-loss/dfp/model_best_val_loss_var.pkl" --output-dir "output/results/experiment2-weighted-loss/dfp"
 python eval.py --arch "dfp-last-conv" --weights "experiment2-weighted-loss/dfp-last-conv/model_best_val_loss_var.pkl" --output-dir "output/results/experiment2-weighted-loss/dfp-last-conv"
-python eval.py --arch "dfp-resnet34-conv" --weights "experiment2-weighted-loss/dfp-resnet34-conv/model_best_val_loss_var.pkl" --output-dir "output/results/experiment2-weighted-loss/dfp-resnet34-conv"
 python eval.py --arch "dfp-resnet34-encoder-conv" --weights "experiment2-weighted-loss/dfp-resnet34-encoder-conv/model_best_val_loss_var.pkl" --output-dir "output/results/experiment2-weighted-loss/dfp-resnet34-encoder-conv"
 python eval.py --arch "deeplabv3" --weights "experiment2-weighted-loss/deeplabv3/model_best_val_loss_var.pkl" --output-dir "output/results/experiment2=weighted-loss/deeplabv3"
-python eval.py --arch "dfp-resnet50" --weights "experiment2-weighted-loss/dfp-resnet50-conv/model_best_val_loss_var.pkl" --output-dir "output/results/experiment2-weighted-loss/dfp-resnet50-conv"
-
+python eval.py --arch "dfp-resnet50" --weights "experiment2-weighted-loss/dfp-resnet50/model_best_val_loss_var.pkl" --output-dir "output/results/experiment2-weighted-loss/dfp-resnet50"
+python eval.py --arch "dfp-resnet34" --weights "experiment2-weighted-loss/dfp-resnet34/model_best_val_loss_var.pkl" --output-dir "output/results/experiment2-weighted-loss/dfp-resnet34"
