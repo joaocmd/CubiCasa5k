7ad0b7de882e450d11e60cd29bb3d46df56037ad
PastelBelem8 <catarina.belem@ist.utl.pt>
2022-09-24 13:54:18 -0700

    Update scripts/run_eval.sh

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+set -e
+
 echo "Running evaluation"
-INPUT_DIR=experiment1-uncertainty-loss
-OUTPUT_DIR=output/results
+INPUT_DIR=runs_cubi/resume-from-scale
+OUTPUT_DIR=output/results-resume-from-scale
 MODEL_TYPE=model_best_val_loss_var.pkl
+SPLIT_CONFIG="--split val.txt"
+DEVICE_CONFIG="--device cuda:0"
+echo $INPUT_DIR $OUTPUT_DIR
 
 # echo "Eval Furukawa Original baseline"
-# python eval.py --arch hg_furukawa_original --weights "${INPUT_DIR}/baseline/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/baseline"
+python eval.py --arch hg_furukawa_original --weights "${INPUT_DIR}/../baseline/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/../baseline" $SPLIT_CONFIG $DEVICE_CONFIG
     
-for INPUT_DIR in   "experiment2-weighted-loss" # "experiment1-uncertainty-loss"
-do
-    OUTPUT_DIR=output/results
-     echo $INPUT_DIR $OUTPUT_DIR
-     python eval.py --arch "dfp" --weights "${INPUT_DIR}/dfp/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp"
-     python eval.py --arch "dfp-last-conv" --weights "${INPUT_DIR}/dfp-last-conv/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp-last-conv"
-     python eval.py --arch "dfp-resnet34" --weights "${INPUT_DIR}/dfp-resnet34/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp-resnet34"
-     python eval.py --arch "dfp-resnet34-encoder-conv" --weights "${INPUT_DIR}/dfp-resnet34-encoder-conv/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp-resnet34-encoder-conv"
-     python eval.py --arch "dfp-resnet50" --weights "${INPUT_DIR}/dfp-resnet50/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp-resnet50"
-     python eval.py --arch "deeplabv3" --weights "${INPUT_DIR}/deeplabv3/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/deeplabv3"
- done
+python eval.py --arch "dfp" --weights "${INPUT_DIR}/dfp/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp" $SPLIT_CONFIG $DEVICE_CONFIG
+python eval.py --arch "dfp-last-conv" --weights "${INPUT_DIR}/dfp-last-conv/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp-last-conv" $SPLIT_CONFIG $DEVICE_CONFIG
+python eval.py --arch "dfp-resnet34" --weights "${INPUT_DIR}/dfp-resnet34/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp-resnet34" $SPLIT_CONFIG $DEVICE_CONFIG
+python eval.py --arch "dfp-resnet34-encoder-conv" --weights "${INPUT_DIR}/dfp-resnet34-encoder-conv/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp-resnet34-encoder-conv" $SPLIT_CONFIG $DEVICE_CONFIG
+python eval.py --arch "dfp-resnet50" --weights "${INPUT_DIR}/dfp-resnet50/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/dfp-resnet50" $SPLIT_CONFIG $DEVICE_CONFIG
+python eval.py --arch "deeplabv3" --weights "${INPUT_DIR}/deeplabv3/${MODEL_TYPE}" --output-dir "${OUTPUT_DIR}/${INPUT_DIR}/deeplabv3" $SPLIT_CONFIG $DEVICE_CONFIG
