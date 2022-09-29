404258a0e3d2a583561cdb0e9bb0e2b6490df7a2
Catarina Belém <PastelBelem8@users.noreply.github.com>
2022-09-29 09:21:53 -0700

    Update run_weighted_train.sh

--- scripts/run_weighted_train.sh
+++ scripts/run_weighted_train.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-python ./train.py --arch deeplabv3 --batch-size 16 --loss weighted
-python ./train.py --arch dfp --batch-size 16 --loss weighted
-python ./train.py --arch dfp-last-conv --batch-size 16 --loss weighted
-python ./train.py --arch dfp-resnet34  --batch-size 16 --loss weighted
-python ./train.py --arch dfp-resnet34-encoder-conv  --batch-size 16 --loss weighted
-python ./train.py --arch dfp-resnet50  --batch-size 4 --loss weighted
+set -e
+
+echo "Training w/ scale"
+python ./train.py --device cuda:3 --min-l-rate -1 --stopping-patience 1000 --scale --arch deeplabv3 --batch-size 16 --loss weighted --n-epoch 100
+
+# TODO
+# - Rename folder w/ resulting model to "runs_cubi/weighted-deeplabv3-with-scale"
+# echo "Resume from scale"
+# python ./train.py --device cuda:3 --min-l-rate -1 --stopping-patience 1000 --scale --arch deeplabv3 --batch-size 16 --loss weighted --resume-epoch --n-epoch 500 --weights "runs_cubi/weighted-deeplabv3-with-scale"
